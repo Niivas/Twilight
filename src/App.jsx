89467a7843390c4c3aa84fb86b1c1817694b2cd5
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-10-20 11:10:24 +0000

    Updated profile details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,17 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nivas Reddy Nalla",
+  title: "Software Engineer & Web developer",
+  email: "nivas6900@gmail.com",
+  gitHub: "https://github.com/Niivas",
+  instagram: "",
+  linkedIn: "https://linkedin.com/in/nivasrn",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
+  leetcode: "https://leetcode.com/nivas6900",
+  hackerrank: "https://hackerrank.com/nivas6900",
 };
 
 const primaryColor = "#4E567E";
